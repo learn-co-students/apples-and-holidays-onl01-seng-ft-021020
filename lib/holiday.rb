39c6f247520fbc6618237b922127aaa7ad0bba3b
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-15 04:47:14 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,9 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday, items|
-      holiday if items.include?("BBQ")
+      if items.include?("BBQ")
+        holiday
+      end
     end
   end.flatten.compact
 end
